056ef1317367ababf5c6017a58b267acd499499e
Finnick223 <filipf192@gmail.com>
2025-02-27 23:49:15 +0100

    refactor: move ToastContainer to providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,13 @@
 'use client';
 import type * as React from 'react';
 import { ReactQueryClientProvider } from './ReactQueryClientProvider';
+import { ToastContainer } from 'react-toastify';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ReactQueryClientProvider>{children}</ReactQueryClientProvider>;
+  return (
+    <ReactQueryClientProvider>
+      {children}
+      <ToastContainer />
+    </ReactQueryClientProvider>
+  );
 }
