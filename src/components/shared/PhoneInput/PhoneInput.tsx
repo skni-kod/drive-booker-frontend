e4a5b0eb95c32e3926335ddba2bf2d8606556dfe
Damian <froispro6969@gmail.com>
2025-04-23 12:20:09 +0200

    refactor: use usePhoneInput to support formatted display

--- src/components/shared/PhoneInput/PhoneInput.tsx
+++ src/components/shared/PhoneInput/PhoneInput.tsx
@@ -2,7 +2,7 @@
 
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import {
   FieldValues,
   Path,
@@ -14,6 +14,7 @@ import {
   CountrySelector,
   defaultCountries,
   parseCountry,
+  usePhoneInput,
 } from 'react-international-phone';
 import 'react-international-phone/style.css';
 
@@ -21,7 +22,7 @@ const ALLOWED_COUNTRIES = ['pl', 'ua', 'de'] as const;
 export type AllowedCountry = (typeof ALLOWED_COUNTRIES)[number];
 
 const isAllowedCountry = (iso2: string): iso2 is AllowedCountry =>
-  (ALLOWED_COUNTRIES as readonly string[]).includes(iso2);
+  ALLOWED_COUNTRIES.includes(iso2 as AllowedCountry);
 
 export interface CustomPhoneInputProps<T extends FieldValues> {
   id: Path<T>;
@@ -29,33 +30,53 @@ export interface CustomPhoneInputProps<T extends FieldValues> {
   register: UseFormRegister<T>;
   error?: string;
   countryName: Path<T>;
+  defaultCountry?: AllowedCountry;
 }
 
 export const PhoneInput = <T extends FieldValues>({
   id,
   label,
-  register,
   error,
   countryName,
+  defaultCountry = 'pl',
 }: CustomPhoneInputProps<T>) => {
-  const { setValue } = useFormContext<T>();
-  const [country, setCountry] = useState<AllowedCountry>('pl');
+  const { setValue, getValues } = useFormContext<T>();
+  const initialCountry = getValues(countryName);
 
   const filteredCountries = useMemo(
     () =>
-      defaultCountries.filter((c) => {
-        const { iso2 } = parseCountry(c);
-        return isAllowedCountry(iso2);
-      }),
+      defaultCountries.filter((c) => isAllowedCountry(parseCountry(c).iso2)),
     [],
   );
 
-  const handleCountryChange = (iso2: string) => {
+  const { inputValue, handlePhoneValueChange, setCountry, country, inputRef } =
+    usePhoneInput({
+      value: getValues(id),
+      defaultCountry: isAllowedCountry(initialCountry)
+        ? initialCountry
+        : defaultCountry,
+      countries: filteredCountries,
+      onChange: ({ phone, country: selectedCountry }) => {
+        if (isAllowedCountry(selectedCountry.iso2)) {
+          setValue(id, phone as PathValue<T, typeof id>);
+          setValue(
+            countryName,
+            selectedCountry.iso2 as PathValue<T, typeof countryName>,
+          );
+        }
+      },
+    });
+
+  const handleCountrySelect = (iso2: string) => {
     if (isAllowedCountry(iso2)) {
       setCountry(iso2);
       setValue(countryName, iso2 as PathValue<T, typeof countryName>);
     }
   };
+
+  const selectedIso2 = isAllowedCountry(country.iso2)
+    ? country.iso2
+    : defaultCountry;
 
   return (
     <div>
@@ -65,15 +86,17 @@ export const PhoneInput = <T extends FieldValues>({
       <div className='relative'>
         <div className='absolute inset-y-0 left-2 flex items-center'>
           <CountrySelector
-            selectedCountry={country}
-            onSelect={({ iso2 }) => handleCountryChange(iso2)}
+            selectedCountry={selectedIso2}
+            onSelect={({ iso2 }) => handleCountrySelect(iso2)}
             countries={filteredCountries}
           />
         </div>
         <Input
           id={id}
           type='tel'
-          {...register(id)}
+          ref={inputRef}
+          value={inputValue}
+          onChange={handlePhoneValueChange}
           className='bg-white pl-16 text-sm sm:text-base'
         />
       </div>
